fec6705dab0b8665670c6b3ce76b128cbdf8c76f
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-03-12 22:14:42 +0000

    Create basic additon function

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,10 +19,12 @@ document.addEventListener('DOMContentLoaded', function() {
             } else {
                 let gameType = this.getAttribute("data-type");
                 console.log('You clicked a button!')
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
 
         })
+        // Call run game function with addition data-type
+        runGame('addition');
 
     }
 })
@@ -32,11 +34,22 @@ document.addEventListener('DOMContentLoaded', function() {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+// Pass gameType variable that gets the datatype from HTML into the function
+function runGame(gameType) {
 
     // Creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    // Check the gameType
+    if (gameType === 'addition') {
+        //Call the function for this game type and pass in the two random numbers
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}! Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -55,7 +68,15 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    //Get operand 1 by ID from the HTML and put in an operand number 
+    document.getElementById('operand1').textContent = operand1;
+
+    //Get operand 2 by ID from the HTML and put in an operand number 
+    document.getElementById('operand2').textContent = operand2;
+
+    // Get operator by ID from HTML and assign + as the operator
+    document.getElementById('operator').textContent = '+';
 
 }
 
